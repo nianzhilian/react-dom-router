59bffaf5fdfe51a4661901548fa14787b1f3ee8a
jpc <jpc@jpcdeMBP.lan>
2021-01-03 23:49:22 +0800

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-16 10:31:02
+ * @LastEditTime: 2020-12-28 10:09:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \webpack-base-config\src\index.jsx
+ */
 import React, { PureComponent } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
